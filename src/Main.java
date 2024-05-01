2f868ef6dc264b22d1238e363e0f964ea64aa5da
Vladimir Muraito <vl.muraito@mail.ru>
2024-05-02 01:20:04 +1000

    Прошу проверить ДЗ.

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,27 @@
-import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("\nTask 1");
         int year = 2021;
         checkYearIsLeapAndPrint(year);
 
-
         System.out.println("\nTask 2");
-        int clientOS = 1;
-        int clientDeviceYear = 2015;
-        System.out.println(validateDeviceAndYearAndPrint(clientOS, clientDeviceYear));
-
+        int clientOS = 0;
+        int clientDeviceYear = 2016;
+        int conclusion = checkingConditionAndReturningString(clientOS, clientDeviceYear);
+        if (conclusion == 1) {
+            System.out.println("«Установите облегченную версию приложения для iOS по ссылке».");
+        } else if (conclusion == 2) {
+            System.out.println("«Установите версию приложения для iOS по ссылке».");
+        } else if (conclusion == 3) {
+            System.out.println("«Установите облегченную версию приложения для Android по ссылке».");
+        } else if (conclusion == 4) {
+            System.out.println("«Установите версию приложения для Android по ссылке».");
+        } else if (conclusion == 5) {
+            System.out.println("«Такая система не поддерживается».");
+        }
 
         System.out.println("\nTask 3");
-        int calculated = calculateDeliveryDistance(49);
+        int calculated = calculateDeliveryDistance(25);
         if (calculated == -1) {
             System.out.println("«Извините, в ваш регион мы не осуществляем доставку».");
         } else {
@@ -32,18 +39,18 @@ public class Main {
         }
     }
 
-    private static String validateDeviceAndYearAndPrint(int deviceType, int year) {
-        int currentYear = LocalDate.now().getYear();
+    private static int checkingConditionAndReturningString(int deviceType, int year) {
+        int currentYear = 2015;
         if (deviceType == 0 && year < currentYear) {
-            return "«Установите облегченную версию приложения для iOS по ссылке».";
+            return 1;
         } else if (deviceType == 0) {
-            return "«Установите версию приложения для iOS по ссылке».";
-        }else if (deviceType == 1 && year < currentYear) {
-            return "«Установите облегченную версию приложения для Android по ссылке».";
+            return 2;
+        } else if (deviceType == 1 && year < currentYear) {
+            return 3;
         } else if (deviceType == 1) {
-            return "«Установите версию приложения для Android по ссылке».";
+            return 4;
         } else {
-            return "«Такая система не поддерживается».";
+            return 5;
         }
     }
 
